6da9f84e0d9875b9b9b1be0164a0e91a89d6713e
Rahfat Chowdhury <91210725+Rahfatc03@users.noreply.github.com>
2021-09-26 22:37:27 -0400

    This is version 1 of my hello world program

---
+++ helloWorld.py
@@ -0,0 +1,7 @@
+Python 3.9.7 (v3.9.7:1016ef3790, Aug 30 2021, 16:39:15) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # This is Version 1 of my hello world program
+>>> print("Hello my name is Rahfat")
+Hello my name is Rahfat
+>>> 
